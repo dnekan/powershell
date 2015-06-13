4ddfeffea00d432b6cdb9f7131ee1d498c10550f
unknown <eskaggs@CATITIM03KBH01.catapultsystems.com>
2015-06-13 09:29:39 -0700

    Initial check in

--- O365-GetSiteCollectionStorageInfo.ps1
+++ O365-GetSiteCollectionStorageInfo.ps1
@@ -4,19 +4,23 @@ Import-Module Microsoft.Online.SharePoint.PowerShell -ErrorAction SilentlyContin
 # Config -- get URL from "Tenant Admin" page in Office 365
 $url	= "https://company-admin.sharepoint.com"
 $user	= "admin@company.com"
-$smtp	= "mailrelay"
-
-# Connect
-Connect-SPOService -url $url -credential $user
+# $smtp	= "mailrelay"
 
 # Gather data
 $sites = Get-SPOSite -Detailed
 $coll = @()
 foreach ($s in $sites) {
+	$surl = $s.Url
 	$curr = $s.StorageUsageCurrent
 	$quota = $s.StorageQuota
-	$prct = [math]::Round($curr/$quota, 2) * 100
-	$surl = $s.Url
+	if($quota -eq 0)
+	{
+		$prct = [math]::Round($curr, 2) * 100
+	}
+	else
+	{
+		$prct = [math]::Round($curr/$quota, 2) * 100
+	}
 	$coll += New-Object -TypeName PSObject -Prop (@{'curr'=$curr;'quota'=$quota;'prct'=$prct;'surl'=$surl})
 }
 $coll = $coll | sort prct -Desc
@@ -24,10 +28,12 @@ $coll = $coll | sort prct -Desc
 # Format HTML
 $html = "<table><tbody><tr><td><b>URL</b></td><td><b>Current (MB)</b></td><td><b>Quota (MB)</b></td><td><b>Percent</b></td><td></td></tr>"
 foreach ($c in $coll) {
-	$row = "<tr><td><a href="{0}/_layouts/15/storman.aspx">{0}</a></td><td>{1}</td><td>{2}</td><td>{3} %</td><td><div style='background-color: blue'>&nbsp;</div><td></td>" -f $c.surl, $c.curr, $c.quota, $c.prct
+	$row = "<tr><td><a target='_blank' href='{0}/_layouts/15/storman.aspx'>{0}</a></td><td>{1}</td><td>{2}</td><td>{3} %</td><td><div style='background-color: blue'>&nbsp;</div><td></td>" -f $c.surl, $c.curr, $c.quota, $c.prct
 	$html += $row
 }
 $html += "</tr></tbody></table>"
 
+$html | Out-File O-365-Tenant-Storage-Report.html
+
 # Send email
-Send-MailMessage -To $user -From $user -Subject "Office 365 Storage" -Body $html -BodyAsHtml -SmtpServer $smtp
+# Send-MailMessage -To $user -From $user -Subject "Office 365 Storage" -Body $html -BodyAsHtml -SmtpServer $smtp

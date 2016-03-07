be916b129dccd9f9f3e2d2587037ba004914fd4f
skaggej <eskaggs@outlook.com>
2016-03-06 23:01:43 -0700

    spo report updates

---
+++ O365-GetTenantAdmins.ps1
@@ -0,0 +1,20 @@
+﻿workflow Get-O365Admins
+{
+	$emailRecipients = "someone@somewhere.com", "someone@somewhere.com", "someone@somewhere.com"
+	$credName = "<Azure credential asset name>"
+	$cred = Get-AutomationPSCredential -Name $credName
+	Connect-MsolService -Credential $cred
+	$html = "<div style='font-family:Calibri, Helvetica, sans-serif; font-size:18pt;'>Office 365 Report - TenantName - Admins</div><br/>"
+	$html += "<table><tr><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'><b>User Name</b></td><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'><b>Admin Role</b></td></tr>"
+	$msolUsers = Get-MsolUser | select userprincipalname | sort userprincipalname
+	foreach ($msolUser in  $msolUsers) { 
+		$upn = $msolUser.userprincipalname
+	    $admin=Get-MsolUserRole -UserPrincipalName $upn
+		$adminName = $admin.name
+		if ($adminName -ne $null -and $adminName -ne "Directory Synchronization Accounts") {
+	        $html += "<tr><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'>" + $upn + "</td><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;''>" + $adminName + "</td></tr>"
+	    }
+	}
+	$html += "</table>"
+	Send-MailMessage -From "someone@somewhere.com" -To $emailRecipients -Subject "Office 365 Report - TenantName - Admins" -Body $html -BodyAsHtml -SmtpServer "smtp.office365.com" -Port 587 -Credential $cred -UseSsl
+}
--- SPO-GetSiteCollectionStorageInfo.ps1
+++ SPO-GetSiteCollectionStorageInfo.ps1
@@ -1,73 +1,18 @@
-<# 
-.SYNOPSIS  
-    Connect to SharePoint Online (SPO) Admin Site and display storage quota and usage information of all provisioned site collections.
-    
-.DESCRIPTION 
-    This runbook displays quota information of all the provisioned SPO site collections for a particular Office 365 tenant.
-Prereq:    
-    1. Automation Module Asset containing SPO integration module.
-    2. Automation Credential Asset containing the admin user id and the password for SPO tenant. 
-    3. The SendMailUsingOffice365 runbook needs to be imported and published within the Azure Automation Account.
-
-.PARAMETER  SPOAdminSiteUrl 
-    String Url to the SPO Admin Site. 
-    Example: https//:TenantName-admin.sharepoint.com 
- 
-.PARAMETER  PSCredName
-    String name of PSCredential Asset. 
-    Example: SPOAdminCred. The asset with name 'SPOAdminCred'' must be present as a Credential asset of type PSCredential. 
-
-.EXAMPLE 
-   Get-SPOStorageInfo -SPOAdminSiteUrl "https://{tenant}-admin.sharepoint.com" -PSCredName "SPOAdminCred"
-
-.NOTES 
-    Author: Jeff Jones, modified by Eric Skaggs 
-    Originally written by Jeff Jones (@spjeff) and posted here:  http://www.spjeff.com/2015/05/18/office-365-monitor-site-collection-storage-ps1-daily-email/.
-    Updated by Eric Skaggs
-    -Minor update to avoid divideByZero error
-    -Minor update to add parameters
-    -Minor update to display values in GB instead of MB.
-    Last Updated: 06/18/2015   
-#> 
 workflow Get-SPOStorageInfo
 {
-   <# 
-	param( 
-	    [OutputType([string[]])]
-	  
-	    [Parameter(Mandatory=$true)]
-	    [string] 
-	    $SPOAdminSiteUrl,
-	    
-	    [Parameter(Mandatory=$true)]   
-	    [string] 
-	    $PSCredName
-	)
-    #>
-	$PSCred = Get-AutomationPSCredential -Name $PSCredName
-	Write-Output "Connecting to SPO Admin Site: $SPOAdminSiteUrl using account:" $PSCred.UserName
-	Connect-SPOService -Url $SPOAdminSiteUrl -Credential $PSCred   
-    
+	$emailRecipients ="someone@somewhere.com", "someone@somewhere.com", "someone@somewhere.com"
+	$credName = "<Azure credential asset name>"
+	$cred = Get-AutomationPSCredential -Name $credName
+	Connect-SPOService -Url https://TenantName-admin.sharepoint.com -Credential $cred   
 	$sites = Get-SPOSite -Detailed
-    $html = "<table><tbody><tr><td><b>URL</b></td><td><b>Current (GB)</b></td><td><b>Quota (GB)</b></td><td><b>Percent</b></td><td></td></tr>"
-    
+	$html = "<div style='font-family:Calibri, Helvetica, sans-serif; font-size:18pt;'>Office 365 Report - TenantName - SharePoint Online Storage</div><br/>"
+    $html += "<table><tbody><tr><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'><b>URL</b></td><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'><b>Current (GB)</b></td><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'><b>Quota (GB)</b></td></tr>"    
 	foreach ($s in $sites) {
     	$surl = $s.Url
     	$curr = [math]::Round($s.StorageUsageCurrent / 1024, 2)
     	$quota = [math]::Round($s.StorageQuota / 1024, 2)
-    	if($quota -eq 0)
-    	{
-    		$prct = [math]::Round($curr, 2) * 100
-    	}
-    	else
-    	{
-    		$prct = [math]::Round($curr/$quota, 2) * 100
-    	}
-        $html += "<tr><td><a target='_blank' href='" + $surl + "/_layouts/15/storman.aspx'>" + $surl + "</a></td><td>" + $curr + "</td><td>" + $quota + "</td><td>" + $prct + " %</td><td><div style='background-color: blue'>&nbsp;</div><td></td>"
+        $html += "<tr><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'><a target='_blank' href='" + $surl + "/_layouts/15/storman.aspx'>" + $surl + "</a></td><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'>" + $curr + "</td><td style='font-family:Calibri, Helvetica, sans-serif; font-size:11pt;'>" + $quota + "</td></tr>"
     }    
-    $html += "</tr></tbody></table>"
-            
-    Write-Output "Starting send email runbook..."
-    SendMailUsingOffice365 -AzureOrgIdCredential $PSCredName -Body $html -to "Someone@Somewhere.com" -from "Someone@Somewhere.com" 
-    Write-Output "Done!"
+    $html += "</tbody></table>"
+	Send-MailMessage -From "someone@somewhere.com" -To $emailRecipients -Subject "Office 365 Report - TenantName - SharePoint Online Storage" -Body $html -BodyAsHtml -SmtpServer "smtp.office365.com" -Port 587 -Credential $cred -UseSsl
 }
